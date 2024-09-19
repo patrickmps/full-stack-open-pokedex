bffa5afc5a048162a5583827b029e3590399d628
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2024-09-19 10:23:34 -0300

    feat: Add end-to-end test for opening Charizard's details page

--- cypress/e2e/pokedex_e2e_test.cy.js
+++ cypress/e2e/pokedex_e2e_test.cy.js
@@ -3,5 +3,14 @@ describe('Pokedex', function () {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  }),
+
+  it('charizard pokemon details page can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('charizard').click()
+    cy.contains('charizard')
+    cy.contains('blaze')
+    cy.contains('solar power')
   })
+
 })
